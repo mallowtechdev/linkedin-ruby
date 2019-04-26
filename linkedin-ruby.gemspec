e032729ee2d0e949c20d4ff8e9f676db21ae2c0d
prakash-subramani <s.prakash.karur@gmail.com>
2019-04-26 19:26:33 +0530

    Oauth2 included

--- linkedin-ruby.gemspec
+++ linkedin-ruby.gemspec
@@ -44,4 +44,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+
+  # We use VCR to mock LinkedIn API calls
+  gem.add_development_dependency "vcr"
+  gem.add_development_dependency "webmock"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,10 @@
-require "bundler/setup"
 require "linkedin-ruby"
 
-RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+# Record and playback LinkedIn API calls
+require 'vcr'
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/vcr_cassettes"
+  config.hook_into :webmock
 end
+
+require 'webmock/rspec'
